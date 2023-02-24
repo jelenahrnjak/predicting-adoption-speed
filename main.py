124bbbfcd518f2224c5430a83d52ab6c25b8bfc9
Kristina Stojic <stojic.kris@gmail.com>
2023-02-24 22:46:39 +0100

    added grid search and F1 score

--- main.py
+++ main.py
@@ -1,123 +1,136 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, GradientBoostingClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 import xgboost as xgb
 import lightgbm as lgb
 
 
-df = pd.read_csv('train.csv')
-print(df.describe())
-df = df.drop(['Name', 'Description', 'RescuerID', 'PetID', 'VideoAmt', 'PhotoAmt'], axis=1)
-
-# Separate the labels and data
-labels = df.columns.tolist()
-data = df.values.tolist()
-
-# Split the data into features and target variable
-X = [row[:-1] for row in data] # All columns except the last one are features
-y = [row[-1] for row in data] # Last column is the target variable
-
-# popravi
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-
-# Train the Random Forest classifier on the training set
-rf = RandomForestClassifier()#n_estimators=2000, n_jobs=-1, random_state=42)
-rf.fit(X_train, y_train)
-
-# Evaluate the performance of the classifier on the validation set
-rf_val_predictions = rf.predict(X_val)
-rf_val_accuracy = accuracy_score(y_val, rf_val_predictions)
-print('Validation accuracy for Random Forest:', rf_val_accuracy)
-
-# Evaluate the performance of the classifier on the test set
-rf_test_predictions = rf.predict(X_test)
-rf_test_accuracy = accuracy_score(y_test, rf_test_predictions)
-print('Test accuracy for Random Forest:', rf_test_accuracy)
-
-# GRADIENT BOOSTING
-
-gb = GradientBoostingClassifier()
-gb.fit(X_train, y_train)
+def load_data(filepath):
+    df = pd.read_csv(filepath)
+    df = df.drop(['Name', 'Description', 'RescuerID', 'PetID', 'VideoAmt', 'PhotoAmt', 'State'], axis=1)
+    data = df.values.tolist()
+    return data
 
 
-gb_val_predictions = gb.predict(X_val)
-gb_val_accuracy = accuracy_score(y_val, gb_val_predictions)
-print('\nValidation accuracy for Gradient Boosting:', gb_val_accuracy)
-
-gb_test_predictions = gb.predict(X_test)
-gb_test_accuracy = accuracy_score(y_test, gb_test_predictions)
-print('Test accuracy for Gradient Boosting:', gb_test_accuracy)
-
-# XGBoost
-
-xgb = xgb.XGBClassifier()
-xgb.fit(X_train, y_train)
+def split_data(data, test_size=0.1, val_size=0.2, random_state=42):
+    X = [row[:-1] for row in data]
+    y = [row[-1] for row in data]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val_size, random_state=random_state)
+    return X_train, X_val, X_test, y_train, y_val, y_test
 
 
-xgb_val_predictions = xgb.predict(X_val)
-xgb_val_accuracy = accuracy_score(y_val, xgb_val_predictions)
-print('\nValidation accuracy for XGBoost:', xgb_val_accuracy)
+def train_random_forest(X_train, y_train): # ovo za sve
+    rf = RandomForestClassifier()
+    best_params = grid_search(X_train, y_train, rf)
+    rf = RandomForestClassifier(**best_params)
+    rf.fit(X_train, y_train)
+    return rf
 
-xgb_test_predictions = xgb.predict(X_test)
-xgb_test_accuracy = accuracy_score(y_test, xgb_test_predictions)
-print('Test accuracy for XGBoost:', xgb_test_accuracy)
+def train_gradient_boosting(X_train, y_train):
+    gb = GradientBoostingClassifier()
+    gb.fit(X_train, y_train)
+    return gb
 
-# Bagging
-bagging = BaggingClassifier()
-bagging.fit(X_train, y_train)
+def train_xgboost(X_train, y_train):
+    xgboost = xgb.XGBClassifier()
+    xgboost.fit(X_train, y_train)
+    return xgboost
 
-bagging_val_predictions = bagging.predict(X_val)
-bagging_val_accuracy = accuracy_score(y_val, bagging_val_predictions)
-print('\nValidation accuracy for Bagging:', bagging_val_accuracy)
+def train_bagging(X_train, y_train):
+    bagging = BaggingClassifier()
+    bagging.fit(X_train, y_train)
+    return bagging
 
-bagging_test_predictions = bagging.predict(X_test)
-bagging_test_accuracy = accuracy_score(y_test, bagging_test_predictions)
-print('Test accuracy for Bagging:', bagging_test_accuracy)
+def train_light_gbm(X_train, y_train, X_val, y_val):
+    X_train2 = np.array(X_train)
+    y_train2 = np.array(y_train)
 
-# LightGBM
+    X_val2 = np.array(X_val)
+    y_val2 = np.array(y_val)
 
-X_train2 = np.array(X_train)
-y_train2 = np.array(y_train)
+    train_data = lgb.Dataset(X_train2, label=y_train2)
+    val_data = lgb.Dataset(X_val2, label=y_val2)
 
-X_val2 = np.array(X_val)
-y_val2 = np.array(y_val)
+    params = {
+        'objective': 'multiclass',
+        'metric': 'multi_logloss',
+        'num_class': 5,
+        'boosting_type': 'gbdt',
+        'num_leaves': 31,
+        'learning_rate': 0.05,
+        'feature_fraction': 0.9,
+        'bagging_fraction': 0.8,
+        'bagging_freq': 5,
+        'n_jobs': -1,
+        'seed': 42
+    }
 
-train_data = lgb.Dataset(X_train2, label=y_train2)
-val_data = lgb.Dataset(X_val2, label=y_val2)
+    # Train the LightGBM model
+    lgb_model = lgb.train(
+        params,
+        train_data,
+        valid_sets=[train_data, val_data],
+        num_boost_round=1000,
+        verbose_eval=False
+    )
 
-params = {
-    'objective': 'multiclass',
-    'metric': 'multi_logloss',
-    'num_class': 5,
-    'boosting_type': 'gbdt',
-    'num_leaves': 31,
-    'learning_rate': 0.05,
-    'feature_fraction': 0.9,
-    'bagging_fraction': 0.8,
-    'bagging_freq': 5,
-    'n_jobs': -1,
-    'seed': 42
-}
+    return lgb_model
 
-# Train the LightGBM model
-lgb_model = lgb.train(
-    params,
-    train_data,
-    valid_sets=[train_data, val_data],
-    num_boost_round=1000,
-)
+def evaluate_model(model, X_val, y_val, X_test, y_test):
+    val_predictions = model.predict(X_val)
+    val_accuracy = accuracy_score(y_val, val_predictions)
+    val_f1 = f1_score(y_val, val_predictions, average='weighted')
+    print('Validation accuracy:', val_accuracy)
+    print('Validation F1 score:', val_f1)
 
-# Evaluate the performance of the model on the validation set
-lgb_val_predictions = lgb_model.predict(X_val)
-lgb_val_predictions = [list(x).index(max(x)) for x in lgb_val_predictions]
-lgb_val_accuracy = accuracy_score(y_val, lgb_val_predictions)
-print('Validation accuracy for LightGBM:', lgb_val_accuracy)
+    test_predictions = model.predict(X_test)
+    test_accuracy = accuracy_score(y_test, test_predictions)
+    test_f1 = f1_score(y_test, test_predictions, average='weighted')
+    print('Test accuracy:', test_accuracy)
+    print('Test F1 score:', test_f1)
 
-# Evaluate the performance of the model on the test set
-lgb_test_predictions = lgb_model.predict(X_test)
-lgb_test_predictions = [list(x).index(max(x)) for x in lgb_test_predictions]
-lgb_test_accuracy = accuracy_score(y_test, lgb_test_predictions)
-print('Test accuracy for LightGBM:', lgb_test_accuracy)
+
+def grid_search(X_train, y_train, rfc):
+    # param_grid = {
+    #     'n_estimators': [299.5, 300, 300.5],
+    #     'max_depth': [34.5, 35, 35.5],
+    #     'min_samples_split': [25.5, 25, 24.5],
+    # }
+
+    param_grid = {
+        'n_estimators': [250, 300, 350], #broj drveca u sumi
+        'max_depth': [20, 35, 45], #maksimalna dubina
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4],
+        'max_features': ['auto', 'sqrt', 'log2'],
+        'bootstrap': [True, False],
+        'random_state': [42]
+    }
+
+    # Create a Grid Search object
+    grid_search = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+
+    # Fit the Grid Search object to the training data
+    grid_search.fit(X_train, y_train)
+
+    # Print the best hyperparameters and corresponding score
+    print('Best hyperparameters:', grid_search.best_params_)
+    print('Best score:', grid_search.best_score_)
+
+    best_params = grid_search.best_params_
+    return best_params
+
+def main():
+    print("u mainu sam")
+    data = load_data('train.csv')
+    X_train, X_val, X_test, y_train, y_val, y_test = split_data(data)
+    model = train_random_forest(X_train, y_train)
+    print('Random forest:')
+    evaluate_model(model, X_val, y_val, X_test, y_test)
+
+if __name__ == '__main__':
+    main()
